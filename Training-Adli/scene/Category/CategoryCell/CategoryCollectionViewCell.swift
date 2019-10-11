e353a552e61bb1356ee2f707d868c07c6e4eac20
set <adli.raihan@wgs.co.id>
2019-10-11 17:16:13 +0700

    Add pagination list of image dashboard

---
+++ Training-Adli/scene/Category/CategoryCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Training-Adli
+//
+//  Created by Stella Patricia on 09/10/19.
+//  Copyright © 2019 Adli Raihan. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Training-Adli/scene/Category/CategoryView/CategorySpikeBill.swift
@@ -0,0 +1,21 @@
+//
+//  CategorySpikeBill.swift
+//  Training-Adli
+//
+//  Created by Stella Patricia on 09/10/19.
+//  Copyright © 2019 Adli Raihan. All rights reserved.
+//
+
+import UIKit
+
+class CategorySpikeBill: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
