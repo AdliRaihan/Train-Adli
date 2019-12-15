afa19b6ddfc05d2912cd20efc8974eecf23ddb99
Adli Raihan <43267304+AdliRaihan@users.noreply.github.com>
2019-12-16 00:57:07 +0700

    Update ConstantVariables.swift

--- Training-Adli/Services/Constant/ConstantVariables.swift
+++ Training-Adli/Services/Constant/ConstantVariables.swift
@@ -15,7 +15,12 @@ class ConstantVariables {
     static var get = ConstantVariables()
     static var baseURL = "https://unsplash.com/"
     static var baseURLAuth = "https://api.unsplash.com/"
-    static var urlToGetAuthorization = "https://unsplash.com/oauth/authorize?client_id=158e22d2be6cab776308c3250270a395cc2813ca8346c59643b949a4c68ae513&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=public+read_user+write_user+read_photos+write_photos+write_likes+write_followers+read_collections+write_collections"
+    static var urlToGetAuthorization = "https://unsplash.com/oauth/"
+    
+    /* in case you dont know the logic 
+     you need to impl url to get auth as webview then get the auth code to make request , after that you need to redirect / 
+    make a route to dashboard , you dont need to set up anything else just get the url 
+    */
     
     static var clientId         = /* change this */ "adli gantenk"
     static var clientIdKey      = "client_id"
