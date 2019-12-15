ad9d6c56c6207c3e62636ceda21ad9874f0dd57a
Adli Raihan <43267304+AdliRaihan@users.noreply.github.com>
2019-12-16 00:55:42 +0700

    remove my client id & etc

--- Training-Adli/Services/Constant/ConstantVariables.swift
+++ Training-Adli/Services/Constant/ConstantVariables.swift
@@ -17,11 +17,11 @@ class ConstantVariables {
     static var baseURLAuth = "https://api.unsplash.com/"
     static var urlToGetAuthorization = "https://unsplash.com/oauth/authorize?client_id=158e22d2be6cab776308c3250270a395cc2813ca8346c59643b949a4c68ae513&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=public+read_user+write_user+read_photos+write_photos+write_likes+write_followers+read_collections+write_collections"
     
-    static var clientId         = "158e22d2be6cab776308c3250270a395cc2813ca8346c59643b949a4c68ae513"
+    static var clientId         = /* change this */ "adli gantenk"
     static var clientIdKey      = "client_id"
-    static var secretKey        = "55c7dc2458c517c02b2797f92f9707f1195e28cf10ab18cb1bf6c238932fc7c3"
+    static var secretKey        = /* change this */ "read api.unsplash.com for setup the environtmen"
     static var secretKeyKey     = "client_secret"
-    static var redirectUrl      = "urn:ietf:wg:oauth:2.0:oob"
+    static var redirectUrl      = ""
     static var redirectUrlKey   = "redirect_uri"
     static var scope            = "public+read_user"
     static var scopeKey         = "scope"
@@ -29,12 +29,3 @@ class ConstantVariables {
     static var responseTypeKey  = "response_type"
     static var accessToken      = Defaults[.userAuthenticationCode]
 }
-
-
-/*
- https://unsplash.com/oauth/authorize?
- client_id=158e22d2be6cab776308c3250270a395cc2813ca8346c59643b949a4c68ae513&
- redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&
- response_type=code&
- scope=public+read_user
- */
